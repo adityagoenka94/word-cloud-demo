48a2e63b184cd2c4a8684b041043fbfc5d86fead
Aditya Goenka <adityagoenka94@gmail.com>
2019-12-05 10:45:27 +0530

    Project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
   title = 'DynamicWordCloud';
+  wordclouds = [
+    { divid: 'wordcloud1', title: 'Word Cloud 1' },
+    { divid: 'wordcloud2', title: 'Word Cloud 2' },
+    { divid: 'wordcloud3', title: 'Word Cloud 3' }
+  ];
+
+ request = new HttpClient('https://api.myjson.com/bins/80zl0');
+
+ fetch(request)
+ .then(blobResponse => blobResponse.json())
+ .then(response => {
+     console.log('data is ', response);
+     httpData = response.data;
+     wordCloudModule.wordCloudGenerator({
+         containerid: '.wordcloudcont',
+         wordclouddata: httpData,
+         stopwords: stopWords
+     });
+ })
+ .catch(error => {
+     console.error(error);
+ });
+
 }
